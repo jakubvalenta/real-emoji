2d5eb2d1566d4400d5dd2b2071194f62d3033b17
Jakub Valenta <jakub@jakubvalenta.cz>
2019-04-22 14:24:55 +0200

    html: Show character names

--- emoji.py
+++ emoji.py
@@ -4,43 +4,69 @@ import csv
 import json
 import os
 import re
+import unicodedata
 from dataclasses import dataclass
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 
 @dataclass
 class Emoji:
     path: str
-    name: str
-    sequence: List[str]
+    string: str
+    custom_name: Optional[str]
 
     @property
-    def html(self) -> str:
-        return ''.join(f'&#x{code};' for code in self.sequence)
+    def name(self) -> Optional[str]:
+        if self.custom_name:
+            return self.custom_name
+        if not self.char:
+            return None
+        try:
+            return unicodedata.name(self.char)
+        except ValueError:
+            return None
 
     @property
-    def sequence_html(self) -> str:
-        return '&#200B;'.join(f'&#x{code};' for code in self.sequence)
+    def char(self) -> Optional[str]:
+        if len(self.string) == 1:
+            return self.string[0]
+        return None
 
     @property
-    def sequence_text(self) -> str:
-        return ' + '.join(f'U+{code}' for code in self.sequence)
+    def sequence(self) -> str:
+        return '\N{ZERO WIDTH SPACE}'.join(self.string)
+
+    @property
+    def code(self) -> str:
+        hex_strs = (hex(ord(char))[2:] for char in self.string)
+        return ' + '.join(f'U+{hex_str.upper()}' for hex_str in hex_strs)
+
+    @property
+    def title(self) -> str:
+        return ' | '.join(x for x in [self.name, self.code] if x)
+
+    @property
+    def standard(self) -> bool:
+        return not self.custom_name
 
     @classmethod
-    def from_path(cls, path: str, names: Dict[str, str]) -> 'Emoji':
-        name = names.get(path)
+    def from_path(cls, path: str, custom_names: Dict[str, str]) -> 'Emoji':
+        custom_name = custom_names.get(path)
         m = re.search(r'^emoji_u(?P<sequence>.*)\.png$', path)
-        sequence = [x.upper() for x in m.group('sequence').split('_')]
-        return cls(path=path, name=name, sequence=sequence)
+        string = ''.join(
+            chr(int(hex_str, 16)) for hex_str in m.group('sequence').split('_')
+        )
+        return cls(path=path, string=string, custom_name=custom_name)
 
     def to_dict(self) -> dict:
         return {
             'path': self.path,
             'name': self.name,
-            'html': self.html,
+            'string': self.string,
             'sequence': self.sequence,
-            'sequence_html': self.sequence_html,
-            'sequence_text': self.sequence_text,
+            'code': self.code,
+            'title': self.title,
+            'standard': self.standard,
         }
 
 
@@ -51,13 +77,13 @@ def main(
 ):
     with open(csv_file) as f:
         reader = csv.reader(f)
-        names = {path + '.png': name for path, name in reader}
+        custom_names = {path + '.png': name for path, name in reader}
     emojis = [
-        Emoji.from_path(path, names).to_dict()
+        Emoji.from_path(path, custom_names).to_dict()
         for path in sorted(os.listdir(png_dir))
     ]
     with open(output_file, 'w') as f:
-        json.dump(emojis, f, sort_keys=True, indent=2)
+        json.dump(emojis, f, ensure_ascii=False, sort_keys=True, indent=2)
 
 
 if __name__ == '__main__':

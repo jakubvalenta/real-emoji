ee947cac1084550bcf705d6427750d4de8a52611
Jakub Valenta <jakub@jakubvalenta.cz>
2019-10-22 15:56:34 +0200

    Add social media meta tags

--- emoji/posts.py
+++ emoji/posts.py
@@ -16,11 +16,13 @@ def main():
         title = emoji['display_name'] or emoji['name']
         datetime_ = f'2019-10-10T00:00:{i:02}+02:00'
         svg_filename = emoji['filename']
+        png_filename = Path(svg_filename).with_suffix('.png')
         post_filename = Path(svg_filename).with_suffix('.html')
         post_path = out_dir_path / post_filename
         post_content = f'''---
 title: {title}
-filename: {svg_filename}
+svg: {svg_filename}
+png: {png_filename}
 date: {datetime_}
 ---
 '''
